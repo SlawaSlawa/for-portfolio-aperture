1827dc5146b7a90dca0bbf98cf3aef1f73f885ba
SlawaSlawa <forstudyandwork.slawa@yandex.ru>
2025-09-30 19:40:33 +0400

    added scripts for burger button

--- js/main.ts
+++ js/main.ts
@@ -1,20 +1,19 @@
-"use strict";
-var bodyEl = document.querySelector("body");
-var headerEl = document.querySelector(".header");
-var burgerBtnEl = document.querySelector(".burger");
-var headerListEl = document.querySelector(".header__list");
+const bodyEl = document.querySelector("body") as HTMLBodyElement;
+const headerEl: HTMLElement | null = document.querySelector(".header");
+const burgerBtnEl: HTMLButtonElement | null = document.querySelector(".burger");
+const headerListEl: HTMLUListElement | null =
+    document.querySelector(".header__list");
+
 if (burgerBtnEl && headerListEl && headerEl) {
-    burgerBtnEl.addEventListener("click", function () {
+    burgerBtnEl.addEventListener("click", function (this: any) {
         if (this.classList.contains("burger--active")) {
             this.classList.remove("burger--active");
             headerListEl.style.right = "-100%";
             headerEl.style.overflow = "hidden";
-        }
-        else {
+        } else {
             this.classList.add("burger--active");
             headerListEl.style.right = "0";
             headerEl.style.overflow = "visible";
         }
     });
 }
-//# sourceMappingURL=main.js.map
