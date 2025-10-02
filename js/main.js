857d0172763b1511153fbe18d246c3ba1e6b953e
SlawaSlawa <forstudyandwork.slawa@yandex.ru>
2025-10-02 16:12:22 +0400

    added validation for form

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 "use strict";
+// -----------BURGER BTN-----------
 var bodyEl = document.querySelector("body");
 var headerEl = document.querySelector(".header");
 var burgerBtnEl = document.querySelector(".burger");
@@ -14,6 +15,37 @@ if (burgerBtnEl && headerListEl && headerEl) {
             this.classList.add("burger--active");
             headerListEl.style.right = "0";
             headerEl.style.overflow = "visible";
+        }
+    });
+}
+// ------------Validatoin Form
+var formEl = document.querySelector(".form");
+var formInputEl = document.querySelector("#form-input");
+var formErrorMessageEl = document.querySelector(".form__error-message");
+var formBtn = document.querySelector(".form__btn");
+var sendFlag = false;
+if (formInputEl && formErrorMessageEl && formBtn) {
+    formInputEl.addEventListener("input", function () {
+        var inputValue = this.value;
+        if (inputValue.trim().length < 3) {
+            formInputEl.classList.add("form__input--error");
+            formErrorMessageEl.style.top = "0";
+            sendFlag = false;
+            formBtn.disabled = true;
+        }
+        else {
+            formInputEl.classList.remove("form__input--error");
+            formErrorMessageEl.style.top = "-100%";
+            sendFlag = true;
+            formBtn.disabled = false;
+        }
+    });
+}
+if (formEl) {
+    formEl.addEventListener("submit", function (evt) {
+        evt.preventDefault();
+        if (sendFlag && formInputEl) {
+            alert("Your message " + formInputEl.value.trim() + " has been sent");
         }
     });
 }
--- js/main.ts
+++ js/main.ts
@@ -1,3 +1,5 @@
+// -----------BURGER BTN-----------
+
 const bodyEl = document.querySelector("body") as HTMLBodyElement;
 const headerEl: HTMLElement | null = document.querySelector(".header");
 const burgerBtnEl: HTMLButtonElement | null = document.querySelector(".burger");
@@ -14,6 +16,45 @@ if (burgerBtnEl && headerListEl && headerEl) {
             this.classList.add("burger--active");
             headerListEl.style.right = "0";
             headerEl.style.overflow = "visible";
+        }
+    });
+}
+
+// ------------Validatoin Form
+const formEl: HTMLFormElement | null = document.querySelector(".form");
+const formInputEl: HTMLInputElement | null =
+    document.querySelector("#form-input");
+const formErrorMessageEl: HTMLElement | null = document.querySelector(
+    ".form__error-message"
+);
+const formBtn: HTMLButtonElement | null = document.querySelector(".form__btn");
+
+let sendFlag: boolean = false;
+
+if (formInputEl && formErrorMessageEl && formBtn) {
+    formInputEl.addEventListener("input", function (this: any) {
+        const inputValue = this.value;
+        if (inputValue.trim().length < 3) {
+            formInputEl.classList.add("form__input--error");
+            formErrorMessageEl.style.top = "0";
+            sendFlag = false;
+            formBtn.disabled = true;
+        } else {
+            formInputEl.classList.remove("form__input--error");
+            formErrorMessageEl.style.top = "-100%";
+            sendFlag = true;
+            formBtn.disabled = false;
+        }
+    });
+}
+
+if (formEl) {
+    formEl.addEventListener("submit", (evt) => {
+        evt.preventDefault();
+        if (sendFlag && formInputEl) {
+            alert(
+                "Your message " + formInputEl.value.trim() + " has been sent"
+            );
         }
     });
 }
